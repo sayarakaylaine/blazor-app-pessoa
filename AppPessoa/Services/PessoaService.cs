10412f87361b4c0a75ae5a88f4d5b6cb698f8353
Sayara Kaylaine <159396381+sayarakaylaine@users.noreply.github.com>
2024-02-28 10:06:13 -0400

    Add files via upload

---
+++ AppPessoa/Services/PessoaService.cs
@@ -0,0 +1,37 @@
+﻿using AppPessoa.Contexto;
+using AppPessoa.Models;
+namespace AppPessoa.Services
+{
+    public class PessoaService
+    {
+        private readonly ContextBD _context;
+
+        public PessoaService(ContextBD cont)
+        {
+            _context = cont;
+        }
+
+        public async Task<List<Pessoa>>? Pessoas()
+        {
+            var pessoas = _context.Pessoas?.ToList();
+            return pessoas;
+        }
+
+        public async Task<Pessoa>? GetPessoa(int id)
+        {
+            var pessoa = _context.Pessoas?.Where(p => p.Id == id).FirstOrDefault();
+            return pessoa;
+        }
+
+        public async Task<Pessoa> GetPessoa(string cpf)
+        {
+            var pessoa = _context.Pessoas?.Where(p => p.Cpf == cpf).FirstOrDefault();
+            return pessoa;
+        }
+
+        public async Task Add(Pessoa pessoa)
+        {
+            _context.Pessoas?.Add(pessoa);
+        }
+    }
+}
